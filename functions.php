8bd55fee34f4e2955b0273b659c5ce16789b5bc3
ayoube1234 <ayoubmmari8@gmail.com>
2022-08-21 17:53:52 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_plugin_add_stylesheet' );
+function my_plugin_add_stylesheet() {
+    wp_enqueue_style( 'my-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all' );
